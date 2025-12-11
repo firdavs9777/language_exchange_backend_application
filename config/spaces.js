efb843187789fbfd7bceaa094838c4eb5674ac25
firdavs9777 <fmutalipov97@gmail.com>
2025-12-11 11:33:08 +0900

    test

--- config/spaces.js
+++ config/spaces.js
@@ -1,13 +1,15 @@
-// config/spaces.js
-const { S3Client } = require('@aws-sdk/client-s3');
 
-const s3Client = new S3Client({
-  endpoint: `https://${process.env.SPACES_ENDPOINT || 'sfo3.digitaloceanspaces.com'}`,
-  region: 'sfo3',
-  credentials: {
-    accessKeyId: process.env.DO_SPACES_KEY,
-    secretAccessKey: process.env.DO_SPACES_SECRET
-  }
+// config/spaces.js
+const AWS = require('aws-sdk');
+
+// Configure DigitalOcean Spaces endpoint
+const spacesEndpoint = new AWS.Endpoint(process.env.SPACES_ENDPOINT || 'sfo3.digitaloceanspaces.com');
+
+const s3 = new AWS.S3({
+  endpoint: spacesEndpoint,
+  accessKeyId: process.env.DO_SPACES_KEY,
+  secretAccessKey: process.env.DO_SPACES_SECRET,
+  // Do NOT set region for DigitalOcean Spaces with AWS SDK v2
 });
 
-module.exports = s3Client;
+module.exports = s3;
