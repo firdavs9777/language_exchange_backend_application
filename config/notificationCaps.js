f4b75ff7710ce13ef8aeb94476e425bbadb48e2d
firdavs9777 <fmutalipov97@gmail.com>
2026-05-04 23:23:22 +0900

    feat(notifications): per-type daily and weekly cap config

---
+++ config/notificationCaps.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  daily: {
+    moment_like: 5,
+    moment_comment: 10,
+    profile_visit: 3,
+    follower_moment: 5,
+    friend_request: 10,
+    comment_reply: 10,
+    comment_reaction: 10,
+    comment_mention: 10,
+  },
+  weekly: {
+    re_engagement: 1,
+    digest: 1,
+  },
+};
